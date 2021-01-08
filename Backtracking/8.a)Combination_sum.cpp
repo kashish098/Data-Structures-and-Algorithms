ac23b4e836e8dfeddb8b6e7b43218be8a8ac2ce2
kashish098 <kashish55533@gmail.com>
2021-01-08 16:48:49 +0530

    8.a)Combination Sum-1

---
+++ Backtracking/8.a)Combination_sum.cpp
@@ -0,0 +1,34 @@
+//PROBLEM LINK
+//https://practice.geeksforgeeks.org/problems/combination-sum-1587115620/1
+
+//DO NOT SUBMIT THIS CODE, THIS IS TO LOOK, IF YOU GET STUCK SOMEWHERE
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void combinationSumUtil(int i, int sum, int target,vector<int> &arr,vector<vector<int> > &combinations,vector<int> &possibleCombination){
+    if(sum == target){
+        combinations.push_back(possibleCombination);
+        return;
+    }
+
+    if(sum > target || i == arr.size()){
+        return;
+    }
+    combinationSumUtil(i+1,sum,target,arr,combinations,possibleCombination);
+    
+    sum += arr[i];
+    possibleCombination.push_back(arr[i]);
+    combinationSumUtil(i+1,sum,target,arr,combinations,possibleCombination);
+    sum -= arr[i];
+    possibleCombination.pop_back();
+}
+
+vector<vector<int> > combinationSum(vector<int> &A, int B) {
+    sort( A.begin(), A.end() );
+    A.erase( unique( A.begin(), A.end() ), A.end() );
+    vector<vector<int> > combinations;
+    vector<int> possibleCombination;
+    combinationSumUtil(0,0,B,A,combinations,possibleCombination);
+    return combinations;
+}
